abdc2198f9513e4de82f815015c36dee52349f21
Mary Bowden <142821814+mary-bowden@users.noreply.github.com>
2025-02-14 16:46:42 +0000

    First proper commit. Heads and templates etc

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,23 @@
 module.exports = {
 
-  // Insert values here
+  "provider1": "Jonanna Barton",
+  "providerEmail1": "jonaan.barton@law.com",
+  "provider1-permiss": "Admin",
 
+  "provider2": "Simi Sunlight",
+  "provider2-permiss": "User",
+
+
+  "caseworker1": "Tattina Wolf",
+  "caseworker1-permiss": "Admin",
+
+  "caseworker2": "Johanna Justice",
+  "caseworker2-permiss": "User",
+
+  "caseworker3": "Ishmale Hussein",
+  "caseworker3-permiss": "User",
+
+  "cwAppStatus": "New",
+
+  "cwAppState": "all",
 }
