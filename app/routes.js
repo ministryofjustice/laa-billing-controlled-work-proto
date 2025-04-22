9eafa234591d2e4cbaf49213fff6a111ddf9ef67
Mary Bowden <142821814+mary-bowden@users.noreply.github.com>
2025-04-22 14:58:23 +0100

    Tweaks to wrap up project

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,15 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+//Continue with import or cancel
+router.post('/submission-import-error', function(request, response) {
+
+  var keepImport = request.session.data['keepImport']
+  if (keepImport == "No"){
+      response.redirect("wip/submissions-submitted")
+  } 
+  else {
+      response.redirect("wip/create/submission-start?submissionType=Legal help&firmAC=C1B1C1&Immigration_matterStart=3&Family_matterStart=2")
+  }
+})
